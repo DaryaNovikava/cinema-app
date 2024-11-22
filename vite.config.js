174547791b67afe79e9cab6ed2a0b27db63625e4
Darya Novikava <dashasaldunova@gmail.com>
2024-11-22 12:10:25 +0100

    use config

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/cinema-app/', // Укажите базовый путь
+  // homepage: 'https://daryanovikava.github.io/cinema-app', // Укажите базовый путь
+  plugins: [react()],
+});
