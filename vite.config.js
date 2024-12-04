a8747a8c2f3a9a25b52586e9768d5dc7a0d9b633
Darya Novikava <dashasaldunova@gmail.com>
2024-12-04 13:57:03 +0100

    use react-infinite-scroll

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/', // Укажите базовый путь
-  // homepage: 'https://daryanovikava.github.io/cinema-app', // Укажите базовый путь
-  plugins: [react()],
-});
